89b96da146c135660c5e40816061a06f15466e32
Aditya Dutta <adityadutta482@gmail.com>
2026-02-03 22:04:33 +0530

    if your eligible to vote or note

---
+++ vote.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int a;
+    printf("enter your age");
+    scanf("%d",&a);
+    if(a>=18){
+        printf("you are eligible to vote ");
+         }
+    else{
+        printf("not eligible to vote ");
+    };
+
+
+    return 0;
+}
