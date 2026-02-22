3185cc2d14ff99a0466acd2882c20ffe3ddb9dda
Aditya Dutta <adityadutta482@gmail.com>
2026-02-23 00:49:50 +0530

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int n,a=0,b=1,c,i;
+    printf("enter the range-");
+    scanf("%d",&n);
+    printf("the series is=");
+    printf("%d %d ",a,b);
+    for(i=1;i<=(n-2);i++){
+        c=a+b;
+        printf("%d ",c);
+         a=b;
+        b=c;
+       
+    
+    }
+  
+    return 0;
+}
+   
