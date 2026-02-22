539ed7635c9a7ce59191135a3029b7741a55afe4
Aditya Dutta <adityadutta482@gmail.com>
2026-02-23 01:11:24 +0530

    Add array input and power calculation program
    Implement a program to read an array of integers, display it, and compute powers for even indexed elements.

---
+++ array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n,i,a[40],b;
+    printf("enter n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("enter numbers");
+        scanf("%d",&a[i]);}
+        printf("the array is\n");
+         for(i=0;i<n;i++){
+             printf("%d ",a[i]);
+         }
+         for(i=0;i<n;i++){
+        if(i%2==0){
+            b=pow(a[i],a[i+1]);}
+            printf("a= %d\n ",b);}
+            return 0;
+}
