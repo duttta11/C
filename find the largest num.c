89e3399e8fad5b691897e3902e253a11a40c3d34
Aditya Dutta <adityadutta482@gmail.com>
2026-03-02 14:58:44 +0530

    Add program to find the largest number in an array
    Implement a program to find the largest number in an array.

---
+++ find the largest num.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int i,j,a[100],t,n;
+    printf("enter the range=");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("enter the numbers in the array");
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(a[j]>a[i]){
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;}}}
+     printf("the largest num is=");           
+    
+        printf("%d",a[0]);
+    
+      return 0;         
+            
+        }
+    
