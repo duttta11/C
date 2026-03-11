8c34ebe390b6cb48337b8e81b3cf67932b22d770
Aditya Dutta <adityadutta482@gmail.com>
2026-03-12 00:01:58 +0530

    Create div.c

---
+++ div.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int i,n,a[100],x;
+    printf("enter range=");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("enter num=");
+        scanf("%d",&a[i]);
+    }
+    printf("enter the num to divide=");
+    scanf("%d",&x);
+    for(i=0;i<n;i++){
+        if(a[i]%x==0){
+            a[i]=a[i]*a[i];
+        }}
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    
+    
+    }
+    return 0;
+}
