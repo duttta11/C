feba026f11df81fea81a847891d45ee9595d0189
Aditya Dutta <adityadutta482@gmail.com>
2026-03-28 16:32:59 +0530

    Create 2darray_sum.c

---
+++ 2darray_sum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int r,c,i,j,s=0;
+    printf("enter rows and cols=");
+    scanf("%d %d",&r,&c);
+    
+    int a[r][c]; 
+    
+    printf("enter elements= \n");
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+            
+        }
+    }
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+         s=s+a[i][j];   
+        }
+    }
+    printf("the sum is %d",s);
+    
+    return 0;
+}
+
