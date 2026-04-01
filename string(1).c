fedc455bf7b47a45d8464071b1d2d57f7402d98f
Aditya Dutta <adityadutta482@gmail.com>
2026-04-01 23:06:48 +0530

    Add string manipulation program in string(1).c
    Implement a program that increments each character in a string by 1.

---
+++ string(1).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char str[20];
+    int l,i;
+    printf("enter the string=");
+    scanf("%s",str);
+    l=strlen(str);
+    for(i=0;i<l;i++){
+        str[i]=str[i]+1;
+    }
+    printf("the string is= %s",str);
+    return 0;
+}
+    
+    
