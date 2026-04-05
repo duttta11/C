248c0046f1241d0aa4dee56c5a020ceecf76d78b
Aditya Dutta <adityadutta482@gmail.com>
2026-04-05 23:15:24 +0530

    Update 2darray_sum.c

--- 2darray_sum.c
+++ 2darray_sum.c
@@ -3,9 +3,7 @@ int main(){
     int r,c,i,j,s=0;
     printf("enter rows and cols=");
     scanf("%d %d",&r,&c);
-    
     int a[r][c]; 
-    
     printf("enter elements= \n");
     for(i=0;i<r;i++){
         for(j=0;j<c;j++){
